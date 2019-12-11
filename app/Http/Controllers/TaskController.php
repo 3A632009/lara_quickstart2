cd11498916777286ed19c0b4ef959caec04dc81a
3A632009 <3a632009@student.ncut.edu.tw>
2019-12-11 10:21:56 +0800

    定義子樣板

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,14 +11,21 @@ class TaskController extends Controller
         $this->middleware('auth');
     }
 
-    /**
- * 顯示使用者所有任務的清單。
- * @param  Request  $request
- * @return Response
- */
+
+    //顯示使用者所有任務的清單。
     public function index(Request $request)
     {
         return view('tasks.index');
+    }
+
+    //建立新的任務
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        // Create The Task...
     }
 }
 
