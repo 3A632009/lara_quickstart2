cb230883ad931fba99261136e8eab306ed36cf93
3A632009 <3a632009@student.ncut.edu.tw>
2019-12-18 11:21:39 +0800

    路由模型綁定

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,9 @@ Route::auth();
 
 Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
-Route::delete('/task/{task}', 'TaskController@destroy');
+
+Route::delete('/task/{task}', 'TaskController@destroy') ;
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
 
